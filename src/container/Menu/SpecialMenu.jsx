a083857197b96fe9ec4f58eb762c41f827693d8c
futhimhlas <futhimhl2@gmail.com>
2022-02-18 10:55:49 +0000

    WIP for special menu html

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { SubHeading, MenuItem } from '../../components';
+import { images, data } from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+      <div className="app__specialMenu-title">
+          <SubHeading title="Menu That Fits Your Palatte"  />
+          <h1 className="headtext__cormorant"> Today's Special</h1>
+      </div>
+
+      <div className="app__specialMenu-menu">
+          <div></div>
+      </div>
   </div>
 );
 
