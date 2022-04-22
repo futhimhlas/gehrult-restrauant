89fdff32156a7433c3bf650e315893628eb6726d
futhimhlas <futhimhl2@gmail.com>
2022-04-22 20:57:22 +0000

    Images and heading imported to Chef component

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="Chef" />
+    </div>
+    <div className="app__wrapper_info">
+
+    </div>
   </div>
 );
 
